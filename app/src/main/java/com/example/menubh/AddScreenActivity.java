687579f0baf0dba9189a0fb76d828739ede0e617
DevDuque <devgduque@gmail.com>
2024-06-13 12:21:22 -0300

    Adiciona a funcionalidade do AddButton e começa a AddScreenActivity.

--- app/src/main/java/com/example/menubh/AddScreenActivity.java
+++ app/src/main/java/com/example/menubh/AddScreenActivity.java
@@ -1,9 +1,6 @@
 package com.example.menubh;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,32 +8,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    private ImageButton AddButton;
+public class AddScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_screen);
 
-        // Responsividade
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        AddButton = findViewById(R.id.addButton);
-
-        AddButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent addScreen = new Intent(MainActivity.this, AddScreenActivity.class);
-
-                startActivity(addScreen);
-            }
         });
     }
 }
